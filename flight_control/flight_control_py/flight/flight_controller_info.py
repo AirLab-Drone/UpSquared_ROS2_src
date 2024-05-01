a95419651cc3ae51a3c1d4212aa9ef5303ea7d9e
orange2000121 <danny1.2104@gmail.com>
2024-05-01 22:27:12 +0800

    取得UWB座標function

--- flight_control/flight_control_py/flight/flight_controller_info.py
+++ flight_control/flight_control_py/flight/flight_controller_info.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Range
+from sensor_msgs.msg import Range, NavSatFix
+from mavros_msgs.msg import GPSRAW
+
 
 class FlightInfo:
     """
@@ -11,10 +13,16 @@ class FlightInfo:
         if not rclpy.ok():
             rclpy.init()
         self.node = node
-        self.subscription_rangefinder_pub = self.node.create_subscription(
+        self.node.create_subscription(
             Range,
             "/mavros/rangefinder_pub",
             self.rangefinderCallback,
+            rclpy.qos.qos_profile_sensor_data,
+        )
+        self.node.create_subscription(
+            GPSRAW,
+            "/mavros/gpsstatus/gps1/raw",
+            self.coordinateCallback,
             rclpy.qos.qos_profile_sensor_data,
         )
         # ------------------------------ set init value ------------------------------ #
@@ -26,9 +34,33 @@ class FlightInfo:
         """
         self.rangefinder_alt = msg.range
 
+    def coordinateCallback(self, msg):
+        """
+        Callback function for the coordinate subscription.
+        """
+        LSB_M_TO_LAT_LONG = 8.993216059e-6
+        LAT_START = 22.5180977
+        LONG_START = 113.9007239
+        self.coordinate = msg
+        lat = msg.lat * 1e-7
+        lon = msg.lon * 1e-7
+        alt = msg.alt * 1e-3
+        x = (lon - LONG_START) / LSB_M_TO_LAT_LONG
+        y = (lat - LAT_START) / LSB_M_TO_LAT_LONG
+        # print(f"lat: {lat}, lon: {lon}, alt: {alt}")
+        print(f"x: {x}, y: {y}, z: {alt}")
+
     def destroy(self):
         """
         Destroys the node and shuts down the ROS 2 system.
         """
         self.node.destroy_node()
         rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    if not rclpy.ok():
+        rclpy.init()
+    node = rclpy.create_node("flight_info_node")
+    flight_info = FlightInfo(node)
+    rclpy.spin(node)
