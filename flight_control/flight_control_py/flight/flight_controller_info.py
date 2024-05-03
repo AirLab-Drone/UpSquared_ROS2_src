12a264231957828ba80349a046a5f74d67e731aa
orange2000121 <danny1.2104@gmail.com>
2024-05-03 16:56:55 +0800

    compass msg

--- flight_control/flight_control_py/flight/flight_controller_info.py
+++ flight_control/flight_control_py/flight/flight_controller_info.py
@@ -2,7 +2,24 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Range, NavSatFix
 from mavros_msgs.msg import GPSRAW
+from std_msgs.msg import Header, Float64
 
+
+class XYZ:
+    __slots__ = ["header", "x", "y", "z"]
+
+    _fields_and_field_types = {
+        "header": "std_msgs/Header",
+        "x": "float",
+        "y": "float",
+        "z": "float",
+    }
+
+    def __init__(self, header=Header(), x=0, y=0, z=0) -> None:
+        self.header = header
+        self.x = x
+        self.y = y
+        self.z = z
 
 class FlightInfo:
     """
@@ -25,8 +42,16 @@ class FlightInfo:
             self.coordinateCallback,
             rclpy.qos.qos_profile_sensor_data,
         )
+        self.node.create_subscription(
+            Float64,
+            "/mavros/global_position/compass_hdg",
+            self.compassCallback,
+            rclpy.qos.qos_profile_sensor_data,
+        )
         # ------------------------------ set init value ------------------------------ #
         self.rangefinder_alt = 0
+        self.uwb_coordinate = XYZ()
+        self.compass_heading = 0
 
     def rangefinderCallback(self, msg):
         """
@@ -37,6 +62,7 @@ class FlightInfo:
     def coordinateCallback(self, msg):
         """
         Callback function for the coordinate subscription.
+        坐標系:x: 向右為正, y: 向前為正, z: 向上為正
         """
         LSB_M_TO_LAT_LONG = 8.993216059e-6
         LAT_START = 22.5180977
@@ -47,9 +73,15 @@ class FlightInfo:
         alt = msg.alt * 1e-3
         x = (lon - LONG_START) / LSB_M_TO_LAT_LONG
         y = (lat - LAT_START) / LSB_M_TO_LAT_LONG
-        # print(f"lat: {lat}, lon: {lon}, alt: {alt}")
         print(f"x: {x}, y: {y}, z: {alt}")
+        self.uwb_coordinate = XYZ(header=msg.header, x=x, y=y, z=alt)
 
+    def compassCallback(self, msg):
+        """
+        Callback function for the compass subscription.
+        """
+        self.compass_heading = msg.data
+        print(f"compass heading: {self.compass_heading}")
     def destroy(self):
         """
         Destroys the node and shuts down the ROS 2 system.
--- flight_control/flight_control_py/flight/mission.py
+++ flight_control/flight_control_py/flight/mission.py
@@ -47,7 +47,7 @@ class Mission:
         max_speed = 0.3  # 速度 單位:公尺/秒
         max_yaw = 15 * 3.14 / 180  # 15度
         downward_speed = -0.2  # the distance to move down
-        #todo 刪除pid控制程式
+        # todo 刪除pid控制程式
         pid_x = PID(
             0.2,
             0,
@@ -84,8 +84,15 @@ class Mission:
                         last_moveup_time = rclpy.clock.Clock().now()
                 # self.controller.setZeroVelocity()
                 continue
-            marker_x, marker_y, marker_z, marker_yaw, _, _ = closest_aruco.getCoordinate()
-            if marker_x is None or marker_y is None or marker_z is None or marker_yaw is None:
+            marker_x, marker_y, marker_z, marker_yaw, _, _ = (
+                closest_aruco.getCoordinate()
+            )
+            if (
+                marker_x is None
+                or marker_y is None
+                or marker_z is None
+                or marker_yaw is None
+            ):
                 if rclpy.clock.Clock().now() - last_moveup_time > rclpy.time.Duration(
                     seconds=0.5
                 ):
@@ -97,7 +104,7 @@ class Mission:
                 continue
             last_moveup_time = rclpy.clock.Clock().now()
             # -------------------------------- PID control ------------------------------- #
-            #todo 刪除pid控制程式
+            # todo 刪除pid控制程式
             move_x = pid_x.PID(
                 marker_x, self.controller.node.get_clock().now().nanoseconds * 1e-9
             )
@@ -112,14 +119,14 @@ class Mission:
             # -------------------------- limit move_x and move_y and move_yaw------------------------- #
             move_x = min(max(-marker_y, -max_speed), max_speed)
             move_y = min(max(-marker_x, -max_speed), max_speed)
-            if((360-marker_yaw)<marker_yaw):
+            if (360 - marker_yaw) < marker_yaw:
                 marker_yaw = -marker_yaw
             move_yaw = min(max(-marker_yaw * 3.14 / 180, -max_yaw), max_yaw)
             # ----------------------------- send velocity command ----------------------------- #
             if (
                 diffrent_distance < 0.03
                 and self.flight_info.rangefinder_alt <= lowest_high
-                and (0<marker_yaw < 5 or 355<marker_yaw<360)
+                and (0 < marker_yaw < 5 or 355 < marker_yaw < 360)
             ):
                 self.controller.setZeroVelocity()
                 print(f"landing high:{self.flight_info.rangefinder_alt}")
@@ -150,3 +157,40 @@ class Mission:
         print("now I want to land=================================")
         while not self.controller.land():
             print("landing")
+
+    def navigateTo(
+        self, destination_x: float, destination_y: float, destination_z: float
+    ):
+        """
+        Function to navigate the drone to a specified location.
+
+        This function uses the PID controller to control the drone's movement.
+        The drone moves towards the specified location until it reaches the location.
+        The drone stops moving once it reaches the location.
+
+        Args:
+            x (float): The x-coordinate of the location.
+            y (float): The y-coordinate of the location.
+            z (float): The z-coordinate of the location.
+
+        Returns:
+            None
+        """
+        # --------------------------------- variable --------------------------------- #
+        max_speed = 0.3
+        max_yaw = 15 * 3.14 / 180
+
+        # --------------------------------- function --------------------------------- #
+        def around(a, b, threshold=0.5):
+            return abs(a - b) < threshold
+
+        while (
+            # 還沒到指定位置時繼續移動
+            not (
+                around(self.flight_info.uwb_coordinate.x, destination_x)
+                and around(self.flight_info.uwb_coordinate.y, destination_y)
+            )
+        ):
+            x_diff = destination_x - self.flight_info.uwb_coordinate.x
+            y_diff = destination_y - self.flight_info.uwb_coordinate.y
+            yaw_diff = math.atan2(y_diff, x_diff)
