4769c2717ac40422d72e700dbd3e18b1c7f7aefc
orange2000121 <danny1.2104@gmail.com>
2024-08-24 15:33:36 +0800

    feat: Add subscription for relative altitude and callback function
    This commit adds a new subscription for the relative altitude topic and a corresponding callback function to handle the received data. This will allow the flight controller to receive and process the relative altitude information from the MAVROS system.

--- flight_control/launch/flight_prepare.launch.py
+++ flight_control/launch/flight_prepare.launch.py
@@ -8,7 +8,7 @@ import os
 from ament_index_python import get_package_share_directory
 
 
-def changeMavrosPublishRate(msg_id):
+def changeMavrosPublishRate():
     return TimerAction(
         period=10.0,
         actions=[
@@ -19,16 +19,35 @@ def changeMavrosPublishRate(msg_id):
                     "mavros",
                     "mav",
                     "sys",
-                    "message-interval",
-                    "--id",
-                    f"{msg_id}",
-                    "--rate",
+                    "rate",
+                    "--all",
                     "100",
                 ],
                 output="screen",
             )
         ],
     )
+# def changeMavrosPublishRate(msg_id):
+#     return TimerAction(
+#         period=10.0,
+#         actions=[
+#             ExecuteProcess(
+#                 cmd=[
+#                     "ros2",
+#                     "run",
+#                     "mavros",
+#                     "mav",
+#                     "sys",
+#                     "message-interval",
+#                     "--id",
+#                     f"{msg_id}",
+#                     "--rate",
+#                     "100",
+#                 ],
+#                 output="screen",
+#             )
+#         ],
+#     )
 
 
 def generate_launch_description():
@@ -49,8 +68,9 @@ def generate_launch_description():
                     "fcu_url": "/dev/ttyACM0",
                 }.items(),
             ),
-            changeMavrosPublishRate(24),
-            changeMavrosPublishRate(33),
-            changeMavrosPublishRate(132),
+            # changeMavrosPublishRate(24),
+            # changeMavrosPublishRate(33),
+            # changeMavrosPublishRate(132),
+            changeMavrosPublishRate(),
         ]
     )
