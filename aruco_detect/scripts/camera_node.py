6e2b0ab976f2668a071e7e3dce9a49ba9af8f103
orange2000121 <danny1.2104@gmail.com>
2024-09-01 23:56:06 +0800

    完成是否開啟自動曝光的請求 TODO: 直接設定曝光值的請求

--- aruco_detect/scripts/camera_node.py
+++ aruco_detect/scripts/camera_node.py
@@ -14,6 +14,7 @@ from cv_bridge import CvBridge
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from std_srvs.srv import SetBool, SetFloat32
 
 
 class CaptureImage(Node):
@@ -34,28 +35,32 @@ class CaptureImage(Node):
         # )
 
         # ----------------------------------- 相機設定 ----------------------------------- #
-        self.cap = cv2.VideoCapture(1)
+        self.cap = cv2.VideoCapture(0)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         self.cap.set(cv2.CAP_PROP_FPS, 180)
         self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1.0)
+        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3.0)
 
         # 取得當前FPS
         self.actual_fps = self.cap.get(cv2.CAP_PROP_FPS)
         self.get_logger().info(f"Actual FPS: {self.actual_fps}")
 
-
-
         self.bridge = CvBridge()
-        self.msg = Image()
 
-
+        self.camera_image_publisher = self.create_publisher(Image, "/camera/image_raw", 10)
+        
         self.frame_count = 0
-        self.start_time = time.time()
+        self.start_time = self.get_clock().now()
         self.fps = 0
 
-        timer_period = 1.0 / 180.0  # 計時器周期設為相機實際 FPS
+        timer_period = 1.0 / self.actual_fps  # 計時器周期設為相機實際 FPS
         self.timer = self.create_timer(timer_period, self.timer_callback)
+
+        self.set_auto_exposure_srv = self.create_service(SetBool, 'set_auto_exposure', self.set_auto_exposure_callback)
+        
+        self.set_exposure_srv = self.create_service(SetFloat, 'set_exposure', self.set_exposure_callback)
 
     def timer_callback(self):
         # 捕捉每一幀
@@ -69,19 +74,36 @@ class CaptureImage(Node):
 
         # FPS 計算
         self.frame_count += 1
-        elapsed_time = time.time() - self.start_time
-        if elapsed_time > 1:
-            self.fps = self.frame_count / elapsed_time
+        current_time = self.get_clock().now()
+        elapsed_time = current_time - self.start_time
+        elapsed_seconds = elapsed_time.nanoseconds / 1e9  # Convert nanoseconds to seconds
+
+        if elapsed_seconds > 0.5:
+            self.fps = self.frame_count / elapsed_seconds
             self.get_logger().info(f"Current FPS: {self.fps:.2f}")
             self.frame_count = 0
-            self.start_time = time.time()
+            self.start_time = current_time
 
         # 在影像上顯示 FPS (可選)
-        cv2.putText(frame, f'FPS: {self.fps:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        # cv2.putText(frame, f'FPS: {self.fps:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         # 發布圖片
         self.msg = self.bridge.cv2_to_imgmsg(frame, "bgr8")
         self.camera_image_publisher.publish(self.msg)
+
+
+    def set_auto_exposure_callback(self, request, response):
+        if request.data:
+            self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1.0)
+            self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3.0)  # 開啟自動曝光
+            self.get_logger().info("Auto exposure enabled.")
+        else:
+            self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3.0)  
+            self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1.0)   # 關閉自動曝光
+            self.get_logger().info("Auto exposure disabled.")
+        response.success = True
+        response.message = "Auto exposure setting updated."
+        return response
 
 
 def main(args=None):
